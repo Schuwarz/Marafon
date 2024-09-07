d3b3a5003428007441ec9a6aecc723cda0c0616c
schuwarz <schuwarz@outlook.com>
2024-09-07 17:41:37 +0500

    created hero section + fixed burger menu

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -6,16 +6,17 @@ const body = document.body
 const burgerOpen = () => {
   btn.classList.toggle('burger--active')
   menu.classList.toggle('header__menu--active')
+  body.classList.toggle('body--fixed')
 }
 
 const burgerClose = () => {
   btn.classList.remove('burger--active')
   menu.classList.remove('header__menu--active')
+  body.classList.remove('body--fixed')
 }
 
 linksParent.addEventListener('click', event => {
   if (event.target && event.target.tagName === 'A') {
-    console.log(1)
     burgerClose()
   }
 })
